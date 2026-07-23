3a46795a9bebedc834e2b125b84e365f799eea1a
Aaron Mendonca <mendoncaaaron27@gmail.com>
2026-07-23 20:16:11 +0530

    Auto backup

--- auto_push.bat
+++ auto_push.bat
@@ -2,17 +2,28 @@
 
 cd /d "C:\Users\mendo\OneDrive\Aaron Personal\SEM V\DL Practical"
 
+echo Auto GitHub Sync Started...
+echo Checking for changes every 60 seconds...
+echo.
+
 :loop
+
+echo [%date% %time%] Checking for changes...
 
 git add .
 
 git diff --cached --quiet
 
 if errorlevel 1 (
+    echo Changes found. Uploading to GitHub...
     git commit -m "Auto backup"
     git push
+    echo Upload complete.
+) else (
+    echo No changes found.
 )
 
-timeout /t 60 >nul
+echo.
+timeout /t 60
 
 goto loop
