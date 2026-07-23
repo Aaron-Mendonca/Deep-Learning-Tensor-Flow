d9aa8d6300f7747b01e70cec098d3163c94969b8
Aaron Mendonca <mendoncaaaron27@gmail.com>
2026-07-23 19:39:12 +0530

    Initial commit

---
+++ Practical 1/1.1.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+ 
+a = tf.constant(2)
+b = tf.constant([2,2,7])
+c = tf.constant([[0,2,7],[2,0,7]])
+d = tf.constant([1.5,2.5])
+
+print ("Scalar :",a)
+print ("Vector :",b)
+print ("Matrix :",c)
+print ("Float Vector :",d)
